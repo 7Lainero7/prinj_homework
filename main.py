edd99eb08e83a919c69cea5548d12662a1505cfe
7Lainero7 <7lainero7@gmail.com>
2024-11-01 13:02:53 +0500

    type#1

---
+++ main.py
@@ -0,0 +1,31 @@
+import easyocr
+
+import streamlit as st
+
+
+def is_string(variable): return isinstance(variable, str)
+
+def read_text_from_photo(reader, photo):
+    detection = reader.readtext(photo)
+    text = ""
+    for row in detection:
+        for el in row:
+            if is_string(el):
+                text += el + "\n"
+    return text
+
+reader = easyocr.Reader(['ru', 'en'])
+st.title("Загрузка фотографии, с которой вы хотите получить текст")
+st.write(f"Тестовая ссылка:")
+st.write("https://static1.wow2print.com/storage/65/gallery/image/1896205816629f8ee1d12786.96956893.webp")
+photo = st.text_input("Вставьте url фотографии")
+st.button("Обработать")
+if photo:
+    st.image(photo)
+    st.title("Вывод текста, считанного с фотографии")
+    text = read_text_from_photo(reader, photo)
+    if text:
+        st.write("Вывод текста выглядит так:")
+        st.write(text)
+    else:
+        st.write("Нейросети не удалось распознать текст")
